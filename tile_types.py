3545e4b4294c4ff763101166b9e93ea24c5c1022
OmarGQ <danielcot8otmail.com>
2023-05-19 15:52:51 -0600

    field of view implemented

--- tile_types.py
+++ tile_types.py
@@ -24,35 +24,69 @@ tile_dt = np.dtype(
         ("walkable", np.bool_),  #True if this tile can be walked over.
         ("transparent", np.bool_),  #True if this tile doesn't block FOV.
         ("dark", graphic_dt),  #Graphics for when this tile is not in FOV.
+        ("light", graphic_dt), # Graphics for when the tile is in FOV.
     ]
 )
 
 
 def new_tile(
-    *,  
+    *,  # Enforce the use of keywords, so that parameter order doesn't matter.
     walkable: int,
     transparent: int,
     dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+    light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
 ) -> np.ndarray:
     """Helper function for defining individual tile types """
-    return np.array((walkable, transparent, dark), dtype=tile_dt)
+    return np.array((walkable, transparent, dark, light), dtype=tile_dt)
 
+
+# SHROUD represents unexplored, unseen tiles
+SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
+
+"""
+tile_dt = np.dtype(
+    [
+        ("walkable", np.bool),  # True if this tile can be walked over.
+        ("transparent", np.bool),  # True if this tile doesn't block FOV.
+        ("dark", graphic_dt),  # Graphics for when this tile is not in FOV.
+        ("light", graphic_dt),  # Graphics for when the tile is in FOV.
+    ]
+)
+"""
 
 floor = new_tile(
-    walkable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 0)),
+    walkable=True, 
+    transparent=True, 
+    dark=(ord(" "), (155, 155, 155), (25, 25, 0)),
+    light=(ord(" "), (255, 255, 255), (50, 50, 0)),
 )
 wall = new_tile(
-    walkable=False, transparent=False, dark=(ord("#"), (255, 255, 255), (50, 50, 0)),
+    walkable=False, 
+    transparent=False, 
+    dark=(ord("#"), (155, 155, 155), (65, 55, 25)),
+    light=(ord("#"), (255, 255, 255), (50, 50, 0)),
 )
 tree = new_tile(
-    walkable=False, transparent=False, dark=(ord("↑"), (55, 255, 55), (50, 50, 0)),
+    walkable=False, 
+    transparent=False, 
+    dark=(ord("↑"), (25, 155, 25), (25, 25, 0)),
+    light=(ord("↑"), (55, 255, 55), (50, 50, 0)),
 )
 water = new_tile(
-    walkable=True, transparent=False, dark=(ord("▓"), (0, 0, 255), (155, 155, 255)),
+    walkable=True, 
+    transparent=True, 
+    dark=(ord("▓"), (0, 0, 155), (75, 75, 155)),
+    light=(ord("▓"), (0, 0, 255), (155, 155, 255)),
 )
 room_floor = new_tile(
-    walkable=True, transparent=False, dark=(ord("█"), (100, 100, 0), (100, 100, 0)),
+    walkable=True, 
+    transparent=True, 
+    dark=(ord("█"), (50, 50, 0), (50, 50, 0)),
+    light=(ord("█"), (100, 100, 0), (100, 100, 0)),
 )
 goal = new_tile(
-    walkable=True, transparent=True, dark=(ord("▲"), (20, 255, 20), (100, 100, 0)),
+    walkable=True, 
+    transparent=True, 
+    dark=(ord("▲"), (10, 155, 10), (50, 50, 0)),
+    light=(ord("▲"), (20, 255, 20), (100, 100, 0)),
 )
