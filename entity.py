930651cce3e8312181dd52069a083cc4af90af39
OmarGQ <danielcot8otmail.com>
2023-05-26 12:11:54 -0600

    scrolls implemented"

--- entity.py
+++ entity.py
@@ -7,6 +7,7 @@ Created on Tue Feb 21 15:51:12 2023
 
 from __future__ import annotations
 import copy
+import math
 from typing import Optional, Tuple, Type, TypeVar, TYPE_CHECKING, Union
 from render_order import RenderOrder
 
@@ -69,6 +70,10 @@ class Entity:
                 if self.parent is self.gamemap:
                     self.gamemap.entities.remove(self)
             self.parent = gamemap
+
+    def distance(self, x: int, y: int) -> float:
+        """Return the distance between the current entity and the given (x, y) coordinate."""
+        return math.sqrt((x - self.x) ** 2 + (y - self.y) ** 2)
 
     def move(self, dx: int, dy: int) -> None:
         # Move the entity by a given amount
--- entity_factories.py
+++ entity_factories.py
@@ -6,7 +6,7 @@ Created on Sat May 20 19:53:48 2023
 """
 
 from components.ai import HostileEnemy
-from components.consumable import HealingConsumable
+from components import consumable
 from components.fighter import Fighter
 from entity import Actor, Item
 from components.inventory import Inventory
@@ -42,5 +42,26 @@ health_potion = Item(
     char="!",
     color=(127, 0, 255),
     name="Health Potion",
-    consumable=HealingConsumable(amount=4),
+    consumable=consumable.HealingConsumable(amount=4),
+)
+
+lightning_scroll = Item(
+    char="~",
+    color=(255, 255, 0),
+    name="Lightning Scroll",
+    consumable=consumable.LightningDamageConsumable(damage=20, maximum_range=5),
+)
+
+confusion_scroll = Item(
+    char="~",
+    color=(207, 63, 255),
+    name="Confusion Scroll",
+    consumable=consumable.ConfusionConsumable(number_of_turns=10),
+)
+
+fireball_scroll = Item(
+    char="~",
+    color=(255, 0, 0),
+    name="Fireball Scroll",
+    consumable=consumable.FireballDamageConsumable(damage=12, radius=3),
 )
